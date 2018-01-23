192833cc3f494e511a30b2c4dbf665f17ab54a95
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-01-23 16:58:53 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 i=input("enter the character")
 if((i>='a' and i<='z') or (i>='A' and i<='Z')):
-    print("%s is alphabet "%i)
+    print("%s is an alphabet "%i)
 else:
     print("%s is not an alphabet "%i)
