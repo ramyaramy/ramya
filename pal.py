f2c8468d4c3fc7c8a1882c88480fd20df6cf03ff
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-01-29 23:53:41 +0530

    Update pal.py

--- pal.py
+++ pal.py
@@ -1,5 +1,5 @@
 n =(input('Enter any number : '))
 if n ==n[::-1]:
-        print('The given number is PALINDROME')
+        print('yes')
 else:
-        print('The given number is NOT a palindrome')
+        print(' NO')
