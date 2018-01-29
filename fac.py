c949b5b3eda01f86d6fb4e50f3214479f62ff65f
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-01-29 23:09:14 +0530

    Create fac.py

---
+++ fac.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+factorial = 1
+if(num<=1000):
+    if num == 0:
+      print("1")
+    else:
+      for i in range(1,num + 1):
+       factorial = factorial*i
+    print(factorial)
