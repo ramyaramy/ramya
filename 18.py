d2ac808189c848142a71e2ddf3cc2f5332bd7cb0
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-01-30 16:38:22 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,13 @@
+x,y=map(int,(input().split(' ')))
+for num in range(x,y + 1):
+
+      order = len(str(num))
+      sum = 0
+      temp = num
+      while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+      if num == sum:
+       print(num)
