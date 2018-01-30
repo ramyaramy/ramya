0cc0c77993cd9988c206ebdb319168aad4241b4e
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-01-30 15:35:10 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,11 @@
+
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+print(lower,"and",upper,"are:")
+
+for num in range(lower,upper + 1):
+
+
+           if (num % 2) == 0:
+
+              print(num)
