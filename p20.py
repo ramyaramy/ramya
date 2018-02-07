d67b3c4c29a68d992af39ee6f5695e484ca97969
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-02-07 12:37:39 +0530

    Create p20.py

---
+++ p20.py
@@ -0,0 +1,11 @@
+def main():
+    str=input()
+    list=[]
+    for i in str:
+        x=ord(i)+3
+        y=chr(n)
+        list.append(y)
+
+    print(''.join(list))
+if __name__ == '__main__':
+    main()
