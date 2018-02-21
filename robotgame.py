21f143fd04b2ac2917e4718a28befc832f91c9ad
ramyaramy <35684737+ramyaramy@users.noreply.github.com>
2018-02-21 22:36:34 +0530

    Update robotgame.py

--- robotgame.py
+++ robotgame.py
@@ -1,36 +1,53 @@
 #move and route acccording to value
-class robot:
-    N=0
-    E=1
-    S=2
-    W=3
-    x=0
-    y=0
-    dir=N
-    def robot(direction):
-        if value=='R':
-            dir=(dir+1)%4
-            print("dir",dir)
-    def robot(move):
-        if dir==N:
-            y+=1
-            print("moved to",y)
-        elif dir==E:
-            x+=1
-            print("moved to",x)
-        elif dir==S:
-            y-=1
-            print("moved to",y)
-        else:
-            x+=1
-            print("moved to",x)
 
-if __name__=='__main__':
-    path=0
-    for i in range(7):
-        path=input(" ")
-    if path=='M':
-        path=robot.robot(direction)
-    elif path=='R':
-        path=robot.robot(move)
-
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char z[20];
+    int count=0;
+    int x=0,y=0;
+    int dir=0;
+    printf("\n************ROBOT************");
+    printf("\nThe matrix has 3*3 boxes");
+    printf("\nEnter the either m or r\n");
+    scanf("%s",z);
+    for(int i = 0;z[i]!='\0';i++)
+    {
+         count=count+1;
+    }
+    for(int i=0;i<count;i++)
+    {
+       if((z[i]=='m')||(z[i]=='M'))
+        {
+         if((dir==0)&&((x-1)!=-1))
+           x=x-1;
+         if((dir==1)&&((y+1)<3))
+           y=y+1;
+         if((dir==2)&&((x+1)<3))
+           x=x+1;
+         if((dir==3)&&((y-1)!=-1))
+           y=y-1;
+         
+        }  
+    
+        if((z[i]=='r')||(z[i]=='R'))
+        {
+            dir=dir+1;
+          if(dir==4)
+             dir=0;
+        }
+        if(((z[i]!='r')&&(z[i]!='R'))&&((z[i]!='m')&&(z[i]!='M')))
+        printf("\ninvalid input %c",z[i]);
+    }
+    if(dir==0)
+        printf("\nThe robot is in north");
+    if(dir==1)
+        printf("\nThe robot is in east");
+    if(dir==2)
+        printf("\nThe robot is in south");
+    if(dir==3)
+        printf("\nThe robot is in west");
+        printf("\nThe position of the robot is %d,%d",x,y);
+    getch();
+}
